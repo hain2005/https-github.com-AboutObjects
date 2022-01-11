2ee592431ce4dee3086a6c85be06dfd28319bab5
Hai Nguyen <hain2005@gmail.com>
2022-01-11 15:47:32 -0600

    Init

--- Sources/MLBBarcode/MLBBarcode.swift
+++ Sources/MLBBarcode/MLBBarcode.swift
@@ -1,6 +1,13 @@
+import SwiftUI
+
 public struct MLBBarcode {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    @available(iOS 14, macOS 11.0, *)
+    public func getBarCode() -> Image {
+        return Image(systemName: "heart.fill")
     }
 }
