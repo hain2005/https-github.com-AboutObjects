d115ad84095ccc5e75b26e757f2f3708784ae810
Hai Nguyen <hain2005@gmail.com>
2022-01-12 09:05:52 -0600

    Fix struct init

--- Sources/MLBBarcode/Model/Ticket.swift
+++ Sources/MLBBarcode/Model/Ticket.swift
@@ -8,19 +8,22 @@
 import Foundation
 
 public struct Ticket {
-    public let BarcodeIndicator : String
-    public let BarcodeType : String
-    public let Number : String
-    public let Section : String
-    public let Row : String
-    public let Seat : String
-    public let BuyerTypeCode : String
-    public let PriceScaleCode : String
-    public let EventID : String
-    public let ScanMediaTypet : String
-    public var TOTP : String
+    public let BarcodeIndicator : String = ""
+    public let BarcodeType : String = ""
+    public let Number : String = ""
+    public let Section : String = ""
+    public let Row : String = ""
+    public let Seat : String = ""
+    public let BuyerTypeCode : String = ""
+    public let PriceScaleCode : String = ""
+    public let EventID : String = ""
+    public let ScanMediaTypet : String = ""
+    public var TOTP : String = ""
     
     public var BarcodeString : String {
         return BarcodeIndicator + BarcodeType
+    }
+    public init() {
+        
     }
 }
