bf4a869dc3c9c277be2a1396a715cf1cf1ca4c9e
Hai Nguyen <hain2005@gmail.com>
2022-01-12 22:36:10 -0600

    clean up

--- Sources/MLBBarcode/View/MLBBarcodeView.swift
+++ Sources/MLBBarcode/View/MLBBarcodeView.swift
@@ -12,37 +12,23 @@ import SwiftUI
 public struct MLBBarcodeView<Content: View>: View {
     @ObservedObject var barcodeViewModel: BarcodeViewModel
     var content: (_ image: Image) -> Content
-    let placeHolder: Image
+    let placeHolder: Image = Image("PDF417")
     let ticket: Ticket
 
     @State var previousURL: URL? = nil
-    @State var imageData: UIImage? //Data = Data()
+    @State var imageData: UIImage?
 
     public init(
         ticket: Ticket,
-        placeHolder: Image,
         barcodeViewModel: BarcodeViewModel,
         content: @escaping (_ image: Image) -> Content
     ) {
         self.ticket = ticket
-        self.placeHolder = placeHolder
         self.barcodeViewModel = barcodeViewModel
         self.content = content
     }
 
     public var body: some View {
-//        DispatchQueue.main.async {
-////            if (self.previousURL != self.imageFetcher.getUrl()) {
-////                self.previousURL = self.imageFetcher.getUrl()
-////            }
-//
-//            if (!self.barcodeViewModel.imageData.isEmpty) {
-//                self.imageData = self.barcodeViewModel.imageData
-//            }
-//        }
-
-        //let uiImage = imageData.isEmpty ? nil : UIImage(data: imageData)
-        
         let image = imageData != nil ? Image(uiImage: imageData!) : nil;
 
 
@@ -68,9 +54,7 @@ public struct MLBBarcodeView<Content: View>: View {
         _ = Timer.scheduledTimer(withTimeInterval: TimeInterval(5), repeats: true) { _ in
         
             print("loadimage")
-                    loadImage()
-        //            if let image = self.generateTicket(myTicket: self.ticket) {
-        //                self.bcImage = image
+            loadImage()
         }
 
     }
