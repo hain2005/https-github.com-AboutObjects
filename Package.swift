a97adacb2e280014655f86afec07cf75a2ac0898
Hai Nguyen <hain2005@gmail.com>
2022-01-14 09:21:06 -0600

    Add package resource

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MLBBarcode",
-            dependencies: []),
+            dependencies: [],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "MLBBarcodeTests",
             dependencies: ["MLBBarcode"]),
