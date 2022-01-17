88f132004651ded2007c1dde567f3c27223592a4
Hai Nguyen <hain2005@gmail.com>
2022-01-16 18:58:05 -0600

    add image

--- Sources/MLBBarcode/View/MLBBarcodeView.swift
+++ Sources/MLBBarcode/View/MLBBarcodeView.swift
@@ -117,14 +117,14 @@ struct MLBBarcodeView_Previews: PreviewProvider {
 @available(iOS 13.0, *)
 extension Image {
     init(packageResource name: String, ofType type: String) {
-//        #if canImport(UIKit)
-//        guard let path = Bundle.module.path(forResource: name, ofType: type),
-//              let image = UIImage(contentsOfFile: path) else {
-//            self.init(name)
-//            return
-//        }
-//        self.init(uiImage: image)
-        #if canImport(AppKit)
+        #if canImport(UIKit)
+        guard let path = Bundle.module.path(forResource: name, ofType: type),
+              let image = UIImage(contentsOfFile: path) else {
+            self.init(name)
+            return
+        }
+        self.init(uiImage: image)
+        #elseif canImport(AppKit)
         guard let path = Bundle.module.path(forResource: name, ofType: type),
               let image = NSImage(contentsOfFile: path) else {
             self.init(name)
