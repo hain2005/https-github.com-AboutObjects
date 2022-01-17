b645aa84415c1e1032c22ac7163b5c6ae6948496
Hai Nguyen <hain2005@gmail.com>
2022-01-17 08:39:41 -0600

    set alignment

--- Sources/MLBBarcode/View/MLBBarcodeView.swift
+++ Sources/MLBBarcode/View/MLBBarcodeView.swift
@@ -35,7 +35,7 @@ public struct MLBBarcodeView<Content: View>: View {
     public var body: some View {
         let image = barcodeImage != nil ? Image(uiImage: barcodeImage!) : nil;
         
-        return VStack() {
+        return VStack(alignment: .center) {
             Text("Barcode Scan Helper")
                 .padding(.bottom, 3)
             Text("Refresh in \(barcodeViewModel.timeStep - currentCount) secs ")
@@ -128,21 +128,21 @@ struct MLBBarcodeView_Previews: PreviewProvider {
 extension Image {
     init(packageResource name: String, ofType type: String) {
         #if canImport(UIKit)
-        guard let path = Bundle.module.path(forResource: name, ofType: type),
-              let image = UIImage(contentsOfFile: path) else {
-            self.init(name)
-            return
-        }
-        self.init(uiImage: image)
+            guard let path = Bundle.module.path(forResource: name, ofType: type),
+                  let image = UIImage(contentsOfFile: path) else {
+                self.init(name)
+                return
+            }
+            self.init(uiImage: image)
         #elseif canImport(AppKit)
-        guard let path = Bundle.module.path(forResource: name, ofType: type),
-              let image = NSImage(contentsOfFile: path) else {
-            self.init(name)
-            return
-        }
-        self.init(nsImage: image)
+            guard let path = Bundle.module.path(forResource: name, ofType: type),
+                  let image = NSImage(contentsOfFile: path) else {
+                self.init(name)
+                return
+            }
+            self.init(nsImage: image)
         #else
-        self.init(name)
+            self.init(name)
         #endif
     }
 }
